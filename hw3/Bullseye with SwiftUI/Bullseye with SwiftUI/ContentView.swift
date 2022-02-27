825bed1316be9f1fe38f04d7f9cdbf9829442b91
yizhuo17.14@gmail.com <yt_z10@berkeley.edu>
2022-02-26 16:20:08 -0800

    done hw3

--- hw3/Bullseye with SwiftUI/Bullseye with SwiftUI/ContentView.swift
+++ hw3/Bullseye with SwiftUI/Bullseye with SwiftUI/ContentView.swift
@@ -8,10 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var num: Double = 25
+    @State var toggle = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+
+            VStack() {
+                HStack() {
+                    VStack() {
+                        Text("High Score")
+                        Text("0")
+                    }
+                    .padding(.leading, 20)
+                    
+                    Spacer()
+                    
+                    VStack() {
+                        Text("Current Level")
+                        Text("1")
+                    }
+                }
+                Spacer()
+                
+            
+                VStack() {
+                    Text("Move the slider to:")
+                        .font(.system(size: 30))
+                    
+                    if toggle{
+                        Text("\(num, specifier: "%.2f")").font(.system(size: 30))
+                    } else {
+                        Text(String(Int(num))).font(.system(size: 30))
+                    }
+                    
+                    Slider(value: $num, in: 0...100).padding()
+                    Button("Check"){
+                        
+                    }
+
+                }
+                Spacer()
+                VStack() {
+                    Text("Exact Mode?")
+                    Toggle("Exact mode", isOn: $toggle).labelsHidden()
+                }
+            }
+
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
