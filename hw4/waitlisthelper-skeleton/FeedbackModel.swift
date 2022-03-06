128da86aa9b1698b46a242aa8d58893ea9e62fef
yizhuo17.14@gmail.com <yt_z10@berkeley.edu>
2022-03-06 01:07:40 -0800

    fixed feedback model

--- hw4/waitlisthelper-skeleton/FeedbackModel.swift
+++ hw4/waitlisthelper-skeleton/FeedbackModel.swift
@@ -1,17 +1,8 @@
 //
 //  FeedbackModel.swift
-//  waitlisthelper-skeleotn
+//  waitlisthelper-skeleton
 //
-//  Created by Jordan Yee on 1/19/22.
+//  Created by Yi T ZZ on 3/6/22.
 //
 
 import Foundation
-
-let feedbackModel =
-    [(90, "You're good 😄"),
-     (60, "You're probably okay ☺️"),
-     (40, "Idk, it's like 50-50 😐"),
-     (20, "You're probably not getting off 😕"),
-     (0, "Rip 😔")]
-
-var test = "test"
